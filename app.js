862973a437a8b6d3f8c14a149617fdeed87bd8c0
sjunejo <sjunejo1@gmail.com>
2017-03-05 18:55:54 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+
+// Setting '/node_modules' as static so that
+// we can access jQuery from node_modules.
+// TODO Determine whether this is ok...
+app.use(express.static(__dirname + '/node_modules'));
+app.use(express.static(__dirname + '/res'));
+
+
+app.get('/', (req, res, next) => {
+    res.sendFile(__dirname + 'res/index.html');
+});
+
+const PORT = 3000;
+
+const server = app.listen(PORT, function(){
+    console.log('App is listening on port %s', PORT);
+})
+
+const io = require('socket.io')(server);
